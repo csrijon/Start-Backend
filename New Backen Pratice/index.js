b930b235245a18c34195cb5ca64cdf88535164f1
csrijon <csrijon92@gmail.com>
2025-01-23 14:51:00 +0530

    pratice day

---
+++ New Backen Pratice/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"))
+
+app.get( "/", (req,res) => {
+  res.send(" i am a backend developer")
+})
+
+app.post("/",(req,res) => {
+  res.send("i am a post request")
+}
+)
+
+app.listen(port,() => {
+  console.log(`app listen on port ${port}`)
+})
